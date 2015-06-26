6a6391e0b34f50a9175a1b18bf856ce1fd22b1ba
Ron Leung <ron.khleung@gmail.com>
2015-06-26 17:36:56 -0400

    added fixtures and controller

--- js/router.js
+++ js/router.js
@@ -1,3 +1,10 @@
 Todos.Router.map(function() {
 	this.resource('todos', {path: '/'});
 });
+
+// model function - returns all existing todos
+Todos.TodosRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('todo');
+  }
+});
